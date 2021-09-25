472e722c44dea8659ac19a9c418899b865e11f84
Valentin Weber <vweber@stud.hs-heilbronn.de>
2021-09-25 21:36:05 +0200

    ADD | functionality to list and represent manifest files

--- ssdk/core/models.py
+++ ssdk/core/models.py
@@ -14,6 +14,8 @@
 """Data Models for steam-scheduled-download-killer."""
 
 from dataclasses import dataclass
+import os
+from typing import List
 
 
 @dataclass(repr=True)
@@ -21,3 +23,21 @@ class SteamLibrary:
     """Container for a Steam Library."""
 
     path: str
+
+    def get_appmanifest_list(self) -> List[str]:
+        """List all appmanifest.acf files in Steam Library Folder."""
+        manifests = [
+            os.path.join(self.path, man) for man in os.listdir(self.path)
+            if os.path.isfile(os.path.join(self.path, man))
+            and man.startswith("appmanifest_") and man.endswith(".acf")
+        ]
+        return manifests
+
+    @property
+    def game_count(self) -> int:
+        """Return number of games found in Steam Library Folder."""
+        return len(self.get_appmanifest_list())
+
+    def __str__(self) -> str:
+        """Return string with library path and number of manifests."""
+        return f"{self.path} (found {self.game_count} games)"
